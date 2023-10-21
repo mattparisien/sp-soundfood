844f7858c607d5c8826c0859039f53a9273410b7
Matt Parisien <matthewparisien4@gmail.com>
2023-10-21 17:57:57 -0400

    Commit

--- SoundfoodPlayerInterface.js
+++ SoundfoodPlayerInterface.js
@@ -3,16 +3,15 @@ class SoundfoodPlayerInterface {
     this.els = Array.from(document.querySelectorAll("[data-player-el]")).reduce(
       (a, v) => ({ ...a, [v.dataset.playerEl]: v })
     );
-    console.log(this.els);
   }
 
   setAttributes(title, shortTitle, guest, releaseDate) {
-    this.root.setAttribute("data-episode-title", title);
-    this.root.setAttribute("data-episode-short-title", shortTitle);
-    this.root.setAttribute("data-episode-guest", guest);
+    this.els.root.setAttribute("data-episode-title", title);
+    this.els.root.setAttribute("data-episode-short-title", shortTitle);
+    this.els.root.setAttribute("data-episode-guest", guest);
 
-    this.title.innerText = shortTitle;
-    this.date.innerText = releaseDate;
+    this.els.title.innerText = shortTitle;
+    this.els.date.innerText = releaseDate;
   }
 }
 
