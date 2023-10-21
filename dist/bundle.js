551cb9e3281cc572bdefa4955c80271ab23a8609
Matt Parisien <matthewparisien4@gmail.com>
2023-10-21 17:58:36 -0400

    Commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -3637,6 +3637,7 @@ class SoundfoodPlayerInterface {
   }
 
   setAttributes(title, shortTitle, guest, releaseDate) {
+    console.log(this.els)
     this.els.root.setAttribute("data-episode-title", title);
     this.els.root.setAttribute("data-episode-short-title", shortTitle);
     this.els.root.setAttribute("data-episode-guest", guest);
