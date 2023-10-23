65b9ba57e08fa3a83749380b50174b0afe181722
Matt Parisien <matthewparisien4@gmail.com>
2023-10-23 08:07:16 -0400

    Commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -21221,7 +21221,7 @@ class PodcastApi extends Module/* default */.Z {
     this.media = "podcast";
     this.entity = "podcastEpisode";
     this.limit = 100;
-    this.proxyUrl = "http://localhost:3000/episodes";
+    this.proxyUrl = "https://sf-proxy-200fa2f17b47.herokuapp.com/episodes";
   }
 
   async getEpisodes() {
